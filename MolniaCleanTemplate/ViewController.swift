b3a5387a2f20e4bf76a9df3e57675d7b68029c3e
lyapinmichael <lyapin.dev@gmail.com>
2023-12-06 22:14:25 +0300

    Initial Commit

---
+++ MolniaCleanTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MolniaCleanTemplate
+//
+//  Created by Ляпин Михаил on 06.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
